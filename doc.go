6a31fe0e865e3aaa956cbc9b9b477e918224fb2f
Dmytro Shamenko <idestis@gmail.com>
2019-11-13 15:04:34 +0100

    Doc file added

---
+++ doc.go
@@ -0,0 +1,10 @@
+// Package gort is small HTTP handler written on Go to receive
+// remote calls to run scripts bundled in Docker containers
+/* Complete documentation you can find in repository
+https://github.com/idestis/gort
+*/
+package gort
+
+// Copyright 2019 Dmytro Shamenko
+// See theL ICENSE files at the top-level directory of this distribution
+// and at https://github.com/idestis/gort/
